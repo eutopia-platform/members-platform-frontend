2265bc1397e42c3c0067eb2b68509a50d3ed9854
Jannis Jorre <jeyj0.plus@gmail.com>
2019-02-12 14:05:12 +0100

    [17557874] (feat): Add Vue and use it for hello world

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <h1>Hello Members Platform!</h1>
+    <p>This page was created with Vue!</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "app",
+  data() {
+    return {
+      msg: "Welcome to Your Vue.js App!"
+    };
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+h1 {
+  color: green;
+}
+</style>
--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
-console.log("Hello Members Platform");
+import Vue from "vue";
+import App from "./App.vue";
+
+new Vue({
+  el: "#app",
+  render: h => h(App)
+});
