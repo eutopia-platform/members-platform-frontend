36449b4ab820618f9004acdb9890903131e3f8c9
William Dry <wpbdry@gmail.com>
2019-02-26 18:40:22 +0100

    WIP [17930516] (feat): implement routing with Vue Router

--- src/App.vue
+++ src/App.vue
@@ -2,43 +2,24 @@
   <div>
     <h1>Hello Eutopia!</h1>
     <p>This page was created with Vue!</p>
-    <button @click="showLandingPage">
-      Landing Page
-    </button>
-    <button @click="showProjectHub">
-      Project Hub
-    </button>
-    <LandingPage v-if="landingPageIsShown"></LandingPage>
-    <ProjectHub v-if="projectHubIsShown"></ProjectHub>
+    <router-link to="/">
+      Show Landing Page
+    </router-link>
+    |
+    <router-link to="/project-hub">
+      Show Project Hub
+    </router-link>
+    <router-view />
   </div>
 </template>
 
 <script>
-import LandingPage from "./app/LandingPage";
-import ProjectHub from "./app/ProjectHub";
-
 const App = {
   name: "App",
   data() {
     return {
-      msg: "Welcome to Your Vue.js App!",
-      landingPageIsShown: true,
-      projectHubIsShown: false
+      msg: "Welcome to Your Vue.js App!"
     };
-  },
-  components: {
-    LandingPage,
-    ProjectHub
-  },
-  methods: {
-    showLandingPage() {
-      this.$data.landingPageIsShown = true;
-      this.$data.projectHubIsShown = false;
-    },
-    showProjectHub() {
-      this.$data.landingPageIsShown = false;
-      this.$data.projectHubIsShown = true;
-    }
   }
 };
 
--- src/index.js
+++ src/index.js
@@ -1,7 +1,22 @@
 import Vue from "vue";
+import VueRouter from "vue-router";
 import App from "./App.vue";
+import LandingPage from "./app/LandingPage";
+import ProjectHub from "./app/ProjectHub";
+
+Vue.use(VueRouter);
+
+const routes = [
+  { path: "/", component: LandingPage },
+  { path: "/project-hub", component: ProjectHub }
+];
+
+const router = new VueRouter({
+  routes // short for `routes: routes`
+});
 
 new Vue({
   el: "#app",
+  router,
   render: h => h(App)
 });
