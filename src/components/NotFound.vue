180a1642833e5cd0114fc782735fe65f5a8cf913
William Dry <wpbdry@gmail.com>
2019-02-26 19:07:45 +0100

    [17930516] (feat): add 404 Not Found page

---
+++ src/components/NotFound.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h1>404 Not Found</h1>
+    <p>
+      The page you are looking for does not exist. Please check that the URL is
+      correct.
+    </p>
+  </div>
+</template>
+
+<script>
+const NotFound = {
+  name: "NotFound"
+};
+
+export default NotFound;
+</script>
+
+<style lang="scss" scoped></style>
--- src/index.js
+++ src/index.js
@@ -3,12 +3,14 @@ import VueRouter from "vue-router";
 import App from "./App.vue";
 import LandingPage from "./app/LandingPage";
 import ProjectHub from "./app/ProjectHub";
+import NotFound from "./components/NotFound";
 
 Vue.use(VueRouter);
 
 const routes = [
   { path: "/", component: LandingPage },
-  { path: "/project-hub", component: ProjectHub }
+  { path: "/project-hub", component: ProjectHub },
+  { path: "*", component: NotFound }
 ];
 
 const router = new VueRouter({
