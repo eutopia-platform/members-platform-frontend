a6c0e5809433df9dca6e725b308dd6c623a36c0f
William Dry <wpbdry@gmail.com>
2019-02-27 14:41:17 +0100

    [17930516] (feat): remove unnecessary stuff from App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,13 @@
 <template>
   <div>
-    <h1>Hello Eutopia!</h1>
-    <p>This page was created with Vue!</p>
-    <router-link to="/">
-      Show Landing Page
-    </router-link>
-    |
-    <router-link to="/project-hub">
-      Show Project Hub
-    </router-link>
     <router-view />
   </div>
 </template>
 
 <script>
 const App = {
-  name: "App",
-  data() {
-    return {
-      msg: "Welcome to Your Vue.js App!"
-    };
-  }
+  name: "App"
 };
 
 export default App;
 </script>
-
-<style lang="scss" scoped>
-h1 {
-  color: green;
-}
-</style>
