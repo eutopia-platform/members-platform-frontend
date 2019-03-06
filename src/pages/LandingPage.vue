d49c895d88d6f1cca28b2dff2932eec4dfc25de6
Mathis Bullinger <bullinger.mathis@gmail.com>
2019-03-06 17:44:57 +0100

    WIP [18473492] (feat) add basic landing page layout in vanilla HTML

--- src/pages/LandingPage.vue
+++ src/pages/LandingPage.vue
@@ -1,22 +1,200 @@
 <template>
-  <div>
-    <h1>Hello Landing Page!</h1>
+  <div class="landing-page">
+    <!-- <h1>Hello Landing Page!</h1>
     <p>Welcome to our super awesome project!</p>
     <router-link to="/project-hub">
       <Button>Go to project hub</Button>
-    </router-link>
+    </router-link> -->
+
+    <!-- Slide 1 -->
+    <div class="pageslide" id="slide-1">
+      <div class="slide-sec cont-sec">
+        <div class="cont-wrap">
+          <h1>Helping Projects succeed</h1>
+          <p>
+            Project hub helps you to identify the most important aspects of your
+            project so you achieve the right goals.
+          </p>
+          <button type="button" name="button">
+            Request early access
+          </button>
+        </div>
+      </div>
+      <div class="slide-sec img-sec"></div>
+    </div>
+    <!-- Slide 2 -->
+    <div class="pageslide" id="slide-2">
+      <div class="slide-sec cont-sec">
+        <div class="cont-wrap">
+          <h1>Vision driven project plan</h1>
+          <div class="vision-point">
+            <h2>1. Setup your vision</h2>
+            <p>Define a ...</p>
+            <hr>
+          </div>
+          <div class="vision-point">
+            <h2>2. Develop a strategy</h2>
+            <p>Make it feasable...</p>
+            <hr>
+          </div>
+          <div class="vision-point">
+            <h2>3. Create your roadmap</h2>
+            <p>Start activity-focused...</p>
+            <hr>
+          </div>
+          <div class="vision-point">
+            <h2>4. Execute and measure</h2>
+            <p>The final step...</p>
+          </div>
+        </div>
+      </div>
+      <div class="slide-sec img-sec"></div>
+    </div>
+    <!-- Slide 3 -->
+    <div class="pageslide" id="slide-3">
+      <div class="slide-sec cont-sec">
+        <div class="cont-wrap">
+          <h1>What others say</h1>
+          <div class="quote-wrap">
+
+          </div>
+          <div class="quote-wrap">
+
+          </div>
+        </div>
+      </div>
+      <div class="slide-sec img-sec"></div>
+    </div>
+    <!-- Mail Slide -->
+    <div class="pageslide" id="slide-mail"></div>
+
   </div>
 </template>
 
 <script>
 import Button from "../components/atomic/Button";
+import PageSlide from '../components/unstructured/PageSlide.vue'
 
 const LandingPage = {
   name: "LandingPage",
   components: {
-    Button: Button
+    Button: Button,
+    PageSlide: PageSlide
   }
 };
 
 export default LandingPage;
 </script>
+
+<style lang="scss" scoped>
+
+  $blue-light: rgb(126, 192, 250);
+  $blue-dark: rgb(89, 109, 238);
+
+  .landing-page {
+    width: 100%;
+    margin: 0;
+    padding: 0;
+  }
+
+  .pageslide {
+    width: 100%;
+    height: 100vh;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-around;
+    box-sizing: border-box;
+    font-family: 'Roboto', sans-serif;
+    font-size: 16px;
+
+    // border-bottom: 1px rgba(100, 100, 100, 0.1) solid;
+
+    .slide-sec {
+      display: block;
+      box-sizing: border-box;
+      overflow: hidden;
+      height: 100%;
+      width: 50%;
+    }
+
+    .cont-sec {
+      background-color: white;
+      padding: 10%;
+      color: #1A1818;
+      hr {
+        width: 50%;
+        background-color: $blue-dark;
+        height: .4em;
+        border: none;
+      }
+
+      .cont-wrap {
+        position: relative;
+        top: 50%;
+        transform: translateY(-50%);
+      }
+      h1 {
+        text-align: center;
+        font-size: 2.2em;
+      }
+    }
+
+    .img-sec {
+      background-color: $blue-light;
+      background-size: contain;
+      background-repeat: no-repeat;
+    }
+  }
+
+  #slide-1 {
+    .cont-sec {
+      p {
+        font-size: 1em;
+      }
+      button {
+        border: none;
+        border-radius: 1em;
+        background-color: $blue-dark;
+        padding: 0.75rem;
+        cursor: pointer;
+        outline: none;
+        transition: box-shadow 0.2s, transform 0.2s;
+        transform: translate(0, 0px);
+        color: white;
+        left: 50%;
+        position: relative;
+        margin-top: 1em;
+        transform: translateX(-50%);
+      }
+    }
+    .img-sec {
+      background-image: url("../../data/img/team.svg");
+    }
+  }
+  #slide-2 {
+    .cont-sec {
+
+    }
+    .img-sec {
+      background-image: url("../../data/img/team2.svg");
+    }
+  }
+  #slide-3 {
+    .cont-sec {
+      .quote-wrap {
+        background-color: grey;
+        width: 100%;
+        height: 20vh;
+        display: block;
+        margin-bottom: 1em;
+      }
+    }
+    .img-sec {
+      background-image: url("../../data/img/team3.svg");
+    }
+  }
+  #slide-mail {
+    background-color: rgb(89, 109, 238);
+  }
+
+</style>
