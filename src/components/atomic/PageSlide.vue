34d8af29b27ffd4db2d9a0da816166f79f9562d3
Mathis Bullinger <bullinger.mathis@gmail.com>
2019-03-08 14:43:33 +0100

    WIP [feat-184734492] (ref): add split view component + strip down slide

--- src/components/atomic/PageSlide.vue
+++ src/components/atomic/PageSlide.vue
@@ -1,11 +1,6 @@
 <template>
   <div class="page-slide">
-    <div class="half half-left">
-      <slot name="left"></slot>
-    </div>
-    <div class="half half-right">
-      <slot name="right"></slot>
-    </div>
+    <slot></slot>
   </div>
 </template>
 
@@ -24,29 +19,5 @@ export default PageSlide;
 .page-slide {
     width: 100%;
     height: 100vh;
-    display: flex;
-    flex-direction: row;
-    justify-content: space-around;
-    box-sizing: border-box;
-    font-family: 'Roboto', sans-serif;
-    font-size: 1rem;
-    border-bottom: 1px red solid;
-
-    .half {
-        width: 50%;
-        display: block;
-        flex-grow: 1;
-        overflow: hidden;
-        box-sizing: border-box;
-    }
-
-    .half-left {
-      padding: 5rem;
-    }
-
-    .half-right {
-      background-color: $c-primary-notsointense;
-    }
 }
-
 </style>
---
+++ src/components/atomic/SplitViewHorizontal.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="splitview-horizontal">
+    <div class="half half-left">
+      <slot name="left"></slot>
+    </div>
+    <div class="half half-right">
+      <slot name="right"></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+const SplitViewHorizontal = {
+  name: "SplitViewHorizontal"
+};
+
+export default SplitViewHorizontal;
+</script>
+
+<style lang="scss" scoped>
+@import "../sharedStyles/colors.scss";
+@import "../sharedStyles/shapes.scss";
+
+.splitview-horizontal {
+    width: 100%;
+    height: 100%;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-around;
+    box-sizing: border-box;
+    border-bottom: 1px red solid;
+
+    .half {
+        width: 50%;
+        display: block;
+        flex-grow: 1;
+        overflow: hidden;
+        box-sizing: border-box;
+    }
+
+    .half-left {
+      padding: 5rem;
+    }
+
+    .half-right {
+      background-color: $c-primary-notsointense;
+    }
+}
+
+</style>
