fab28974adb5eafb7d0fe38245659cb1bfd0ee0d
Mathis Bullinger <bullinger.mathis@gmail.com>
2019-03-10 22:00:30 +0100

    WIP [feat-184734492] (feat): add social media icons

--- src/pages/landingPage/LpFooter.vue
+++ src/pages/landingPage/LpFooter.vue
@@ -6,6 +6,10 @@
     <router-link to="/privacy">
       Privacy Policy
     </router-link>
+    <img class="image-right" src="../../../data/img/social/medium.svg">
+    <img class="image-right" src="../../../data/img/social/twitter.svg">
+    <img class="image-right" src="../../../data/img/social/facebook.svg">
+    <img class="image-right" src="../../../data/img/social/linkedin.svg">
   </div>
 </template>
 
@@ -34,10 +38,25 @@
       text-decoration: none;
       margin: 1em;
       margin-top: 0;
+      margin-bottom: 0;
       color: white;
       float: left;
       text-align: center;
       line-height: 100px;
+    }
+
+    img {
+      float: right;
+      height: 2.5em;
+      color: white;
+      margin-left: .5em;
+      margin-top: calc(100px / 2 - 2em / 2);
+      filter: invert(100%); // TODO: get logos in right color
+
+      &:nth-of-type(3) { // TODO: resize facebook logo
+        height: calc(2.5em * 0.85);
+        transform: translateY(2.5em * (1 - 0.85) / 2);
+      }
     }
   }
 </style>
