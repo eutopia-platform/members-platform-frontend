7e49347e2a241ad78d483f61be83d0251a16a3cb
Jannis Jorre <jeyj0.plus@gmail.com>
2019-04-03 20:40:42 +0200

    [EUT-50] (feat): add overlay for workspace (no features notification)

--- src/components/atomic/SplitViewHorizontal.vue
+++ src/components/atomic/SplitViewHorizontal.vue
@@ -11,62 +11,61 @@
 
 <script>
 export default {
-  name: 'SplitViewHorizontal'
-}
+  name: "SplitViewHorizontal"
+};
 </script>
 
 <style lang="scss" scoped>
-@import '../sharedStyles/colors.scss';
-@import '../sharedStyles/shapes.scss';
-@import '../sharedStyles/responsive.scss';
+@import "../sharedStyles/colors.scss";
+@import "../sharedStyles/shapes.scss";
+@import "../sharedStyles/responsive.scss";
 
 .splitview-horizontal {
-    width: 100%;
-    height: 100%;
-    display: flex;
-    flex-direction: row;
-    justify-content: space-around;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-around;
+  box-sizing: border-box;
+
+  @media screen and (max-width: $break-mobile) {
+    flex-direction: column;
+    height: 150vh;
+    height: auto;
+  }
+
+  .half {
+    width: 50%;
+    display: block;
+    flex-grow: 1;
+    overflow: hidden;
     box-sizing: border-box;
 
+    img {
+      max-width: 100%;
+      max-height: 100%;
+      position: relative;
+      left: 50%;
+      top: 50%;
+      transform: translateX(-50%) translateY(-50%);
+      z-index: 1;
+    }
+
     @media screen and (max-width: $break-mobile) {
-      flex-direction: column;
-      height: 150vh;
-      height: auto;
+      width: 100%;
+      display: block;
     }
+  }
 
-    .half {
-        width: 50%;
-        display: block;
-        flex-grow: 1;
-        overflow: hidden;
-        box-sizing: border-box;
-
-        img {
-          max-width: 100%;
-          max-height: 100%;
-          position: relative;
-          left: 50%;
-          top: 50%;
-          transform: translateX(-50%) translateY(-50%);
-          z-index: 1;
-        }
-
-        @media screen and (max-width: $break-mobile) {
-          width: 100%;
-          display: block;
-        }
-    }
-
-    .half-left {
-      min-height: 100vh;
-      z-index: 5;
-    }
+  .half-left {
+    min-height: 100vh;
+    z-index: 5;
+  }
 
   .half-right {
     @media screen and (max-width: $break-mobile) {
       height: 50vh;
     }
-
   }
 }
 </style>
--- src/pages/Workspace.vue
+++ src/pages/Workspace.vue
@@ -2,25 +2,33 @@
   <div class="workspace">
     <MenuSidebar></MenuSidebar>
     <Initiative></Initiative>
+    <Overlay v-if="showOverlay"></Overlay>
   </div>
 </template>
 
 <script>
-import Header from '../components/atomic/Header.vue'
-import MenuSidebar from './workspace/Sidebar.vue'
-import Initiative from './workspace/Initiative.vue'
+import Header from "../components/atomic/Header.vue";
+import MenuSidebar from "./workspace/Sidebar.vue";
+import Initiative from "./workspace/Initiative.vue";
+import Overlay from "./workspace/Overlay";
 
 export default {
   name: "Workspace",
   components: {
     Header,
     MenuSidebar,
-    Initiative
+    Initiative,
+    Overlay
+  },
+  data: function() {
+    return {
+      showOverlay: !(this.$route.query.withoutOverlay === null)
+    };
   },
   mounted: function() {
-    console.log(document.cookie)
+    console.log(document.cookie);
   }
-}
+};
 </script>
 
 <style lang="scss" scoped>
