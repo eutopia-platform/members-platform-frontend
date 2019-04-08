b388d7afedffe6c3205808542cf1860ab5e1e2f3
Jannis Jorre <jeyj0.plus@gmail.com>
2019-04-08 13:21:02 +0200

    [EUT-49] (ref): format ViewMilestone

--- src/pages/onboarding/views/ViewMilestone.vue
+++ src/pages/onboarding/views/ViewMilestone.vue
@@ -1,18 +1,17 @@
 <template>
   <ViewBase v-bind:img="image">
     <div>
-      <Header type="secondary">
-        What milestone are you working on at the moment?
-      </Header>
-      <Paragraph>
-        A milestone brings together the key aspects of a successful initiative - your teammates, objectives, key results, toolkits and frameworks so you and your team achieve more, important goals.
-      </Paragraph>
+      <Header type="secondary">What milestone are you working on at the moment?</Header>
+      <Paragraph>A milestone brings together the key aspects of a successful initiative - your teammates, objectives, key results, toolkits and frameworks so you and your team achieve more, important goals.</Paragraph>
       <div class="input-wrap">
-        <Input look="blend" @valueChange="onInputValueChange" @submit="onSubmit"
-          placeholder="Your next big thing" :focus="true"></Input>
-        <Button @click="onSubmit" :disabled="!inputValid">
-          Next
-        </Button>
+        <Input
+          look="blend"
+          @valueChange="onInputValueChange"
+          @submit="onSubmit"
+          placeholder="Your next big thing"
+          :focus="true"
+        ></Input>
+        <Button @click="onSubmit" :disabled="!inputValid">Next</Button>
       </div>
     </div>
     <template v-slot:dashboard>
@@ -25,7 +24,7 @@
 </template>
 
 <script>
-import ViewBase from '../ViewBase.vue'
+import ViewBase from "../ViewBase.vue";
 
 export default {
   name: "ViewMilestone",
@@ -36,5 +35,5 @@ export default {
   computed: {
     image: () => require("../../../../data/img/onboarding/dashboard-1.svg")
   }
-}
+};
 </script>
