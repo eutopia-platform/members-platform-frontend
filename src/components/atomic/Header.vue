7385489ed095660590f5a596ade39dc971826533
Mathis Bullinger <bullinger.mathis@gmail.com>
2019-04-14 21:39:09 +0200

    [EUT-53] (ref): add "Atomic" class for abstraction of atomic components type system

--- src/components/atomic/Header.vue
+++ src/components/atomic/Header.vue
@@ -1,23 +1,17 @@
 <template>
-  <component :is="getTag" :class="getClass" class="header">
+  <component :is="tag" :class="getClass">
     <slot/>
   </component>
 </template>
 
 <script>
-const types = ['primary', 'secondary', 'tertiary', 'quaternary']
-export default {
+import Atomic from '/components/sharedScripts/atomic'
+
+export default new Atomic({
   name: 'Header',
-  props: Object.fromEntries(types.map(t => [t, Boolean])),
-  computed: {
-    type: (self = this) => types.map(t => ([t, self[t]])).find(t => t[1])[0],
-    getClass: (self = this) =>
-      types.map(t => self[t]).includes(true)
-        ? Object.fromEntries(types.map(t => [t, self[t]]))
-        : {[types[0]]: true},
-    getTag: (self = this) => 'h' + (types.indexOf(self.type) + 1)
-  }
-};
+  types: ['primary', 'secondary', 'tertiary', 'quaternary'],
+  tags: ['h1', 'h2', 'h3', 'h4']
+})
 </script>
 
 <style lang="scss" scoped>
@@ -26,19 +20,9 @@ export default {
 @import "../sharedStyles/text.scss";
 
 .header {
-  padding: 0;
-  font-size: 1rem;
   font-weight: bold;
   margin-top: .5em;
   margin-bottom: .5em;
-  display: block;
-
-  img {
-    height: 1.5em;
-    transform: translateY(.25em);
-    margin-right: .5em;
-    display: inline-block;
-  }
 }
 
 .primary {
---
+++ src/components/sharedScripts/debug.js
@@ -0,0 +1,15 @@
+/*
+  ASSERTION FUNCTION - exception will be raised on failure
+  if condition is passed as function, it will not be evaluated in production mode
+  failure message is optional
+*/
+export const assert = process.env.NODE_ENV === 'development'
+  ? (expr, msg) => {
+    if (!ALLOW_EVALUATED && typeof(expr) !== 'function')
+      throw new Error('assert of evaluated expressions is deactivated')
+    if (typeof(expr) === 'function' ? !expr() : !expr)
+      throw new Error(msg || 'assertion failed' + (typeof(expr) === 'function' ? `: ${expr}` : ''))
+    }
+  : () => true
+
+const ALLOW_EVALUATED = true
