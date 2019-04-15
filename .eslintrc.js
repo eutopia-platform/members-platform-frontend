b3959567664c35bf5b02ec6012c3210763f428fa
Jannis Jorre <jeyj0.plus@gmail.com>
2019-04-15 19:05:06 +0200

    [EUT-24] (feat): activate additional, sensible vue rules for eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -16,6 +16,29 @@ const config = {
     'vue/html-closing-bracket-newline': false,
     'vue/html-indent': false,
     'vue/html-self-closing': false,
+    'vue/singleline-html-element-content-newline': false,
+    'vue/multiline-html-element-content-newline': false,
+    'vue/array-bracket-spacing': 'always',
+    'vue/arrow-spacing': { before: true, after: true },
+    'vue/block-spacing': 'always',
+    'vue/component-name-in-template-casing': [
+      'error',
+      'PascalCase',
+      { registeredComponentsOnly: false },
+    ],
+    'vue/comma-dangle': 'only-multiline',
+    'vue/no-empty-pattern': true,
+    'vue/match-component-file-name': [
+      'error',
+      {
+        extensions: ['vue', 'js', 'jsx'],
+        shouldMatchCase: true,
+      },
+    ],
+    'vue/no-boolean-default': ['error', 'no-default'],
+    'vue/object-curly-spacing': ['error', 'always'],
+    'vue/space-infix-ops': ['error', { int32Hint: false }],
+    'vue/v-on-function-call': ['error', 'never'],
   },
 }
 
