4ae3f172f9c3c07ee8c7c3f3c5b5c577bf2d8397
Jannis Jorre <jeyj0.plus@gmail.com>
2019-04-23 16:31:49 +0200

    [EUT-24] (fix): remove unnecessary import in Introduction

--- src/pages/landingPage/Introduction.vue
+++ src/pages/landingPage/Introduction.vue
@@ -1,18 +1,12 @@
 <template>
   <div class="intro">
-    <Header primary>
-      From idea to product
-    </Header>
-    <Tagline type="sub"
-      >Here you find the processes and toolkits you need to turn your ideas into
-      products people want.</Tagline
-    >
+    <Header primary>From idea to product</Header>
+    <Tagline type="sub">
+      Here you find the processes and toolkits you need to turn your ideas into
+      products people want.
+    </Tagline>
     <div class="lean-tool">
-      <ToolkitCard
-        name="Lean Startup Toolkit"
-        banner="full cycle program"
-        :img="img"
-      ></ToolkitCard>
+      <ToolkitCard name="Lean Startup Toolkit" banner="full cycle program" :img="img"></ToolkitCard>
     </div>
   </div>
 </template>
@@ -20,7 +14,6 @@
 <script>
 import Header from '../../components/atomic/Header'
 import Tagline from '../../components/atomic/Tagline'
-import Paragraph from '../../components/atomic/Paragraph'
 import ToolkitCard from '../../components/atomic/ToolkitCard'
 
 export default {
@@ -28,7 +21,6 @@ export default {
   components: {
     Header,
     Tagline,
-    Paragraph,
     ToolkitCard,
   },
   computed: {
