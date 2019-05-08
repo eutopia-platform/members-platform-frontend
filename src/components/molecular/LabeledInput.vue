c9681d016860733dea9a3d48a05638c33065c7c4
Mathis Bullinger <bullinger.mathis@gmail.com>
2019-05-08 14:36:43 +0200

    [CUBE-7] (feat): add labeled input molecular component

---
+++ src/components/molecular/LabeledInput.vue
@@ -0,0 +1,35 @@
+<template>
+  <div :class="getClass">
+    <Paragraph>{{ label }}:</Paragraph>
+    <Input small :size="size" />
+  </div>
+</template>
+
+<script>
+import Molecular from '../sharedScripts/molecular'
+
+export default new Molecular({
+  name: 'LabeledInput',
+  props: {
+    label: {
+      type: String,
+      default: 'unknown',
+    },
+    size: {
+      default: undefined,
+      type: Number,
+    },
+  },
+})
+</script>
+
+<style lang="scss" scoped>
+.labeled-input {
+  display: flex;
+  align-items: baseline;
+
+  *:first-child {
+    margin-right: 1rem;
+  }
+}
+</style>
