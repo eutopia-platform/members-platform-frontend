2e3a09d17923ac5821d18d230d356a25c0c69234
Mathis Bullinger <bullinger.mathis@gmail.com>
2019-05-21 18:15:56 +0200

    [CUBE-25] (feat): dashboard process card placeholder

--- src/pages/workspace/dashboard/Process.vue
+++ src/pages/workspace/dashboard/Process.vue
@@ -1,14 +1,43 @@
 <template>
-  <DashboardCard title="Process Overview"></DashboardCard>
+  <DashboardCard title="Process Overview">
+    <div class="content">
+      <ProcessCard></ProcessCard>
+      <div class="placeholder">
+        <div>
+          <Paragraph>You have not started a process yet.</Paragraph>
+          <Paragraph>
+            We recomment this process to start working on an idea.
+          </Paragraph>
+        </div>
+      </div>
+    </div>
+  </DashboardCard>
 </template>
 
 <script>
 import DashboardCard from './DashboardCard'
+import ProcessCard from './ProcessCard'
 
 export default {
   name: 'Process',
   components: {
     DashboardCard,
+    ProcessCard,
   },
 }
 </script>
+
+<style lang="scss" scoped>
+.content {
+  display: flex;
+  flex-direction: row;
+
+  .placeholder {
+    flex-grow: 1;
+    text-align: center;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-around;
+  }
+}
+</style>
---
+++ src/pages/workspace/dashboard/ProcessCard.vue
@@ -0,0 +1,27 @@
+<template>
+  <Card :class="getClass">
+    <Icon
+      src="https://s3.eu-central-1.amazonaws.com/eutopia.media/process/process_1.svg"
+    ></Icon>
+    <Header quaternary>Build and launch an MVP</Header>
+  </Card>
+</template>
+
+<script>
+import Component from '/components/sharedScripts/component'
+
+export default new Component({
+  name: 'ProcessCard',
+})
+</script>
+
+<style lang="scss" scoped>
+.process-card {
+  box-sizing: border-box;
+  height: 10rem;
+  width: 15rem;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+}
+</style>
