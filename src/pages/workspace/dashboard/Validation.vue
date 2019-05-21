91aeecf1d9a88fe157543c4f9a24e7ccd8b4e310
Mathis Bullinger <bullinger.mathis@gmail.com>
2019-05-21 17:44:43 +0200

    [CUBE-25] (feat): dashboard validation card placeholder

--- src/pages/workspace/dashboard/Validation.vue
+++ src/pages/workspace/dashboard/Validation.vue
@@ -1,6 +1,9 @@
 <template>
   <DashboardCard title="Ready for Validation">
     <div></div>
+    <Paragraph v-if="toolkits.length === 0" class="nokit">
+      No toolkit ready for validation.
+    </Paragraph>
   </DashboardCard>
 </template>
 
@@ -12,5 +15,17 @@ export default {
   components: {
     DashboardCard,
   },
+  data() {
+    return {
+      toolkits: [],
+    }
+  },
 }
 </script>
+
+<style lang="scss" scoped>
+.nokit {
+  margin-top: 3rem;
+  text-align: center;
+}
+</style>
