948fb80030d5bc3fe5e72bbb6686f956076201df
Mathis Bullinger <bullinger.mathis@gmail.com>
2019-05-30 16:15:27 +0200

    [CUBE-27] (ref): simplify header styles

--- src/components/atomic/Header.vue
+++ src/components/atomic/Header.vue
@@ -25,76 +25,36 @@ export default new Atomic({
   font-weight: bold;
   margin-top: 0.5em;
   margin-bottom: 0.5em;
+  line-height: var(--baseline);
+  margin-top: var(--baseline);
+  margin-bottom: var(--baseline);
+}
+
+@for $i from 1 through 5 {
+  .s#{$i} {
+    @include fluid(
+      font-size,
+      $rem-min * pow($fs-scale-min, 4 - $i),
+      $rem-max * pow($fs-scale-max, 4 - $i),
+      $fs-min-screen,
+      $fs-max-screen
+    );
+  }
 }
 
 .s1 {
-  @include fluid(
-    font-size,
-    $rem-min * pow($fs-scale-min, 3),
-    $rem-max * pow($fs-scale-max, 3),
-    $fs-min-screen,
-    $fs-max-screen
-  );
   line-height: calc(2 * var(--baseline));
-  margin-top: var(--baseline);
-  margin-bottom: var(--baseline);
 }
 
 .s2 {
-  @include fluid(
-    font-size,
-    $rem-min * pow($fs-scale-min, 2),
-    $rem-max * pow($fs-scale-max, 2),
-    $fs-min-screen,
-    $fs-max-screen
-  );
-  line-height: calc(2 * var(--baseline));
-  @media screen and (max-width: 500px) {
-    line-height: var(--baseline);
+  @media screen and (min-width: 500px) {
+    line-height: calc(2 * var(--baseline));
   }
-  margin-top: var(--baseline);
-  margin-bottom: var(--baseline);
 }
 
 .s3 {
-  @include fluid(
-    font-size,
-    $rem-min * pow($fs-scale-min, 1),
-    $rem-max * pow($fs-scale-max, 1),
-    $fs-min-screen,
-    $fs-max-screen
-  );
-  line-height: calc(2 * var(--baseline));
-  @media screen and (max-width: 850px) {
-    line-height: var(--baseline);
+  @media screen and (min-width: 850px) {
+    line-height: calc(2 * var(--baseline));
   }
-  margin-top: var(--baseline);
-  margin-bottom: var(--baseline);
-}
-
-.s4 {
-  @include fluid(
-    font-size,
-    $rem-min * pow($fs-scale-min, 0),
-    $rem-max * pow($fs-scale-max, 0),
-    $fs-min-screen,
-    $fs-max-screen
-  );
-  line-height: var(--baseline);
-  margin-top: var(--baseline);
-  margin-bottom: var(--baseline);
-}
-
-.s5 {
-  @include fluid(
-    font-size,
-    $rem-min * pow($fs-scale-min, -1),
-    $rem-max * pow($fs-scale-max, -1),
-    $fs-min-screen,
-    $fs-max-screen
-  );
-  line-height: var(--baseline);
-  margin-top: var(--baseline);
-  margin-bottom: var(--baseline);
 }
 </style>
