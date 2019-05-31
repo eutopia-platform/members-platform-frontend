505589fa37c61c8b176ea9789c2f3df0e7c98633
Mathis Bullinger <bullinger.mathis@gmail.com>
2019-05-31 19:15:46 +0200

    [CUBE-27] (feat): add DataTable component

---
+++ src/components/molecular/table/Cell.vue
@@ -0,0 +1,45 @@
+<template>
+  <div
+    class="table-cell"
+    :class="{ 'border-bottom': borderBottom, 'border-right': borderRight }"
+  >
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+import Atomic from '/components/sharedScripts/atomic'
+
+export default new Atomic({
+  name: 'TableCell',
+  props: {
+    borderBottom: {
+      type: Boolean,
+      default: false,
+    },
+    borderRight: {
+      type: Boolean,
+      default: false,
+    },
+  },
+})
+</script>
+
+<style lang="scss" scoped>
+@import '/components/sharedStyles/colors';
+
+.table-cell {
+  * {
+    margin-top: calc(var(--baseline) / 2) !important;
+    margin-bottom: calc(var(--baseline) / 2) !important;
+  }
+}
+
+.border-bottom {
+  border-bottom: 1px solid color('on-surface');
+}
+
+.border-right {
+  border-right: 1px solid color('on-surface');
+}
+</style>
---
+++ src/components/molecular/table/Table.vue
@@ -0,0 +1,41 @@
+<template>
+  <div :class="getClass" :style="layout">
+    <Cell v-for="(e, i) in columns" :key="i" border-bottom>
+      <Paragraph>{{ e }}</Paragraph>
+    </Cell>
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+import Molecular from '/components/sharedScripts/Molecular'
+import Cell from './Cell'
+
+export default new Molecular({
+  name: 'Table',
+  props: {
+    columns: {
+      type: Array,
+      required: false,
+    },
+  },
+  computed: {
+    layout() {
+      return {
+        'grid-template-columns': '1fr '.repeat(this.columns.length),
+      }
+    },
+  },
+  components: {
+    Cell,
+  },
+})
+</script>
+
+<style lang="scss" scoped>
+.table {
+  display: grid;
+  margin-top: var(--baseline);
+  margin-bottom: var(--baseline);
+}
+</style>
