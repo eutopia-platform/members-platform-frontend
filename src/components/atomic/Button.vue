72ca66dc5224c008d30176e5bee71cb3f8b98523
Mathis Bullinger <bullinger.mathis@gmail.com>
2019-06-01 19:45:18 +0200

    [CUBE-27] (feat): add neutral default button

--- src/components/atomic/Button.vue
+++ src/components/atomic/Button.vue
@@ -9,7 +9,7 @@ import Atomic from '../sharedScripts/atomic'
 
 export default new Atomic({
   name: 'Button',
-  types: ['default', 'secondary'],
+  types: ['default', 'primary', 'secondary'],
   props: {
     img: String,
   },
@@ -21,20 +21,31 @@ export default new Atomic({
 @import '/components/sharedStyles/shadows';
 
 .button {
-  border: none;
   font-size: 1rem;
   min-width: 2rem;
   background-color: color('surface');
-  border: 2px solid color('primary');
-  color: color('primary');
+  border: 0.125rem solid color('on-surface');
+  border-radius: 0.25rem;
+  color: color('on-surface');
   height: calc(1.5 * var(--baseline));
   margin-top: calc(var(--baseline) - var(--baseline) / 4);
   margin-bottom: calc(var(--baseline) - var(--baseline) / 4);
   padding-left: 0.5rem;
   padding-right: 0.5rem;
-  border-radius: 0.25rem;
+  padding-top: 0;
+  padding-bottom: 0;
   cursor: pointer;
   transition: color, background-color 0.2s ease;
+
+  &:hover {
+    background-color: color('on-surface');
+    color: color('surface');
+  }
+}
+
+.primary {
+  border: 2px solid color('primary');
+  color: color('primary');
 
   &:hover {
     background-color: color('primary');
