3472c518518f2531131d2871c16ed8cb9291a5e0
Mathis Bullinger <bullinger.mathis@gmail.com>
2019-06-01 19:58:08 +0200

    [CUBE-27] (feat): add ActionInput component

---
+++ src/components/molecular/ActionInput.vue
@@ -0,0 +1,31 @@
+<template>
+  <div :class="getClass">
+    <Input @focus="setFocus(true)" @blur="setFocus(false)" />
+    <Button ref="button">submit</Button>
+  </div>
+</template>
+
+<script>
+import Molecular from '/components/sharedScripts/component'
+
+export default new Molecular({
+  name: 'ActionInput',
+  methods: {
+    setFocus(status) {
+      this.$refs.button.$el.classList.toggle('primary', status)
+    },
+  },
+})
+</script>
+
+<style lang="scss" scoped>
+.action-input {
+  * {
+    position: relative;
+  }
+
+  .button {
+    transform: translateX(-100%);
+  }
+}
+</style>
