080ec1256f58bdbc16f3be675ffb8b15b352d63b
Mathis Bullinger <bullinger.mathis@gmail.com>
2019-06-02 16:19:06 +0200

    [CUBE-27] (feat): add new components to demo page

--- src/pages/demo/Components.vue
+++ src/pages/demo/Components.vue
@@ -10,15 +10,36 @@
     <Big>Big</Big>
     <Big extra>Big (extra)</Big>
     <Button>Button</Button>
+    <Button primary>Button (primary)</Button>
     <Button secondary>Button (secondary)</Button>
+    <Break></Break>
+    <Input />
+    <Input default-value="with default" />
+    <ActionInput></ActionInput>
+    <ActionInput label="with label"></ActionInput>
+    <ActionInput label="with label" default-value="with default"></ActionInput>
+    <LabeledInput label="label"></LabeledInput>
+    <LabeledInput label="label" default-value="with default"></LabeledInput>
+    <EmailSignup></EmailSignup>
+    <Icon name="close" small></Icon>
+    <Icon name="close"></Icon>
+    <Icon name="close" big></Icon>
   </div>
 </template>
 
 <script>
 import Component from '/components/sharedScripts/component'
+import ActionInput from '/components/molecular/ActionInput'
+import LabeledInput from '/components/molecular/LabeledInput'
+import EmailSignup from '/components/molecular/EmailSignup'
 
 export default new Component({
   name: 'Components',
+  components: {
+    ActionInput,
+    LabeledInput,
+    EmailSignup,
+  },
 })
 </script>
 
