8a70f07dc366d8e3541277cc34ab2eb2735ca59e
Mathis Bullinger <bullinger.mathis@gmail.com>
2019-06-09 15:35:48 +0200

    [CUBE-31] (feat): add margin to template

--- src/pages/workspace/canvas/template.js
+++ src/pages/workspace/canvas/template.js
@@ -16,20 +16,25 @@ export default class Template {
       'template needs to specify width or height'
     )
 
+    const margin = 5
+
     const offLeft = Math.min(...template.boxes.map(box => box.x))
     const offTop = Math.min(...template.boxes.map(box => box.y))
     const scale =
       'width' in template.meta
         ? (template.meta.width * vp.width) /
-          Math.max(...template.boxes.map(box => box.w - offLeft))
+            Math.max(...template.boxes.map(box => box.w - offLeft)) -
+          margin * 2
         : (template.meta.height * vp.height) /
-          Math.max(...template.boxes.map(box => box.h - offTop))
+            Math.max(...template.boxes.map(box => box.h - offTop)) -
+          margin * 2
     const spacing = template.meta.spacing || 0
     vp.offX =
       -(
         vp.width -
         Math.max(...template.boxes.map(box => box.w - offLeft)) * scale
       ) / 2
+    vp.offY -= margin
 
     return {
       boxes: template.boxes.map(box => [
