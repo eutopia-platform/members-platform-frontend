c729d870f6daf608c25deca63d203fb9619cac71
Mathis Bullinger <bullinger.mathis@gmail.com>
2019-06-09 23:34:33 +0200

    [CUBE-35] (feat): add signup step 1 (email & password)

--- src/pages/Signup.vue
+++ src/pages/Signup.vue
@@ -1,12 +1,18 @@
 <template>
-  <div :class="getClass"></div>
+  <div :class="getClass">
+    <Step1></Step1>
+  </div>
 </template>
 
 <script>
 import Component from '/components/sharedScripts/component'
+import Step1 from './signup/Step1'
 
 export default new Component({
   name: 'Signup',
+  components: {
+    Step1,
+  },
 })
 </script>
 
