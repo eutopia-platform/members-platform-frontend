eeef35bb9b129f88fc6d6a0a76a813f3a4b7abf5
Mathis Bullinger <bullinger.mathis@gmail.com>
2019-06-15 12:43:02 +0200

    [dev] (feat): better workspace guard

--- src/pages/workspace/WorkspaceGuard.vue
+++ src/pages/workspace/WorkspaceGuard.vue
@@ -6,82 +6,50 @@
 import Workspace from '~/pages/Workspace'
 import NotFound from '~/pages/NotFound'
 import LoadingScreen from '~/pages/LoadingScreen'
-import gql from 'graphql-tag'
+import queryUserSpaces from '~/gql/workspaces'
+import queryWorkspace from '~/gql/workspace'
 
 export default {
   name: 'WorkspaceGuard',
   apollo: {
-    workspace: {
-      client: 'work',
-      query: gql`
-        query currentWorkspace($spaceUrl: String!) {
-          workspace(name: $spaceUrl) {
-            name
-          }
-        }
-      `,
-      variables() {
-        return {
-          spaceUrl: this.$route.params.workspace,
-        }
-      },
-      skip() {
-        return !this.$route.params.workspace
-      },
-      result({ data }) {
-        this.showWorkspace = data.workspace !== null
-        if (data.workspace) {
-          this.$apollo.provider.clients.work.cache.writeData({ data })
-          localStorage.setItem('workspace', data.workspace.name)
-          if (this.$route.params.workspace !== data.workspace.name)
-            history.replaceState(
-              {},
-              null,
-              this.$route.path
-                .toLowerCase()
-                .replace(data.workspace.name.toLowerCase(), data.workspace.name)
-            )
-        }
-      },
-      error(err) {
-        if (
-          err.message.startsWith('Network error') ||
-          err.graphQLErrors.some(err => err.message.startsWith('Network error'))
-        ) {
-          this.$emit('error', err)
-          return true
-        }
-        if (
-          err.message.replace('GraphQL error:', '').trim() === 'NOT_LOGGED_IN'
-        ) {
-          this.$router.push('/login')
-          return true
-        } else throw Error(err)
-      },
-    },
     workspaces: {
       client: 'work',
-      query: gql`
-        query workspaceList {
-          workspaces {
-            name
-          }
-        }
-      `,
-      result({ data: { workspaces } }) {
-        if (workspaces.length === 0) return
-        this.$router.push('/space/' + workspaces[0].name)
-      },
-      error(err) {
-        if (
-          err.message.replace('GraphQL error:', '').trim() === 'NOT_LOGGED_IN'
-        ) {
-          this.$router.push('/login')
-          return true
-        } else throw Error(err)
-      },
       skip() {
-        return typeof this.$route.params.workspace !== 'undefined'
+        return (
+          this.$route.params.workspace !== undefined ||
+          'workspace' in localStorage
+        )
+      },
+      query: queryUserSpaces,
+      result({
+        data: {
+          workspaces: [{ name: space }],
+        },
+      }) {
+        this.$router.push({
+          path: space,
+          append: true,
+        })
+      },
+      errorPolicy: 'ignore',
+    },
+    workspace: {
+      client: 'work',
+      skip() {
+        return this.$route.params.workspace === undefined
+      },
+      query: queryWorkspace,
+      result({
+        data: {
+          workspace: { name },
+        },
+      }) {
+        localStorage.setItem('workspace', name)
+      },
+      variables() {
+        return {
+          name: this.$route.params.workspace,
+        }
       },
     },
   },
@@ -90,14 +58,18 @@ export default {
     LoadingScreen,
     NotFound,
   },
-  data: () => ({
-    showWorkspace: false,
-  }),
   computed: {
     component() {
       if (this.$apollo.loading) return 'LoadingScreen'
-      return this.showWorkspace ? 'Workspace' : 'NotFound'
+      return this.workspace ? 'Workspace' : 'NotFound'
     },
+  },
+  created() {
+    if (!this.$route.params.workspace && localStorage.getItem('workspace'))
+      this.$router.push({
+        path: localStorage.getItem('workspace'),
+        append: true,
+      })
   },
 }
 </script>
