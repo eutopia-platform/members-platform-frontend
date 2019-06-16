2bba79afa9ad388473e4d192147cf457caceae4e
Mathis Bullinger <bullinger.mathis@gmail.com>
2019-06-16 17:57:14 +0200

    [CUBE-46] (feat): store user loading state

--- src/store/user/index.js
+++ src/store/user/index.js
@@ -18,13 +18,18 @@ export default {
     [types.SET_INFO](state, payload) {
       Object.assign(state.info, payload)
     },
+    [types.SET_LOADING](state, value) {
+      state.loading = value
+    },
   },
   actions: {
     async loadUser({ commit }) {
+      commit(types.SET_LOADING, true)
       const {
         data: { user },
       } = await api.user.query({ query: currentUserQuery })
       commit(types.SET_INFO, user)
+      commit(types.SET_LOADING, false)
     },
   },
 }
