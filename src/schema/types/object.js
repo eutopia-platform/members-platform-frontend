0c8a6656939cf1a7b6382c52ba60d94fa1031b5b
Mathis Bullinger <bullinger.mathis@gmail.com>
2019-06-18 22:21:24 +0200

    [CUBE-46] (feat): schema object at optional props & exclusive switch

--- src/schema/types/object.js
+++ src/schema/types/object.js
@@ -5,17 +5,46 @@ export default class Object_ extends Type_ {
   constructor() {
     super()
     this._entries = {}
+    this._optional = {}
+    this._exclusive = false
   }
 
   check(v) {
     if (typeof v !== 'object') return false
-    if (!haveSameKeys(v, this._entries)) return false
-    return Object.keys(this._entries).every(key =>
-      this._entries[key].check(v[key])
+
+    if (!Object.keys(this._entries).every(e => Object.keys(v).includes(e)))
+      return false
+
+    const allChecks = {
+      ...this._entries,
+      ...this._optional,
+    }
+
+    if (
+      this._exclusive &&
+      !Object.keys(v).every(e => Object.keys(allChecks).includes(e))
+    )
+      return false
+
+    return Object.entries(v).every(e =>
+      Object.keys(allChecks).includes(e[0]) ? allChecks[e[0]].check(e[1]) : true
     )
   }
 
   entries(entr) {
+    return this._addTo(entr, this._entries)
+  }
+
+  optional(entr) {
+    return this._addTo(entr, this._optional)
+  }
+
+  exclusive() {
+    this._exclusive = true
+    return this
+  }
+
+  _addTo(entr, target) {
     assert(typeof entr === 'object')
     assert(
       Object.values(entr).every(v => v instanceof Type_),
@@ -23,23 +52,11 @@ export default class Object_ extends Type_ {
     )
     for (const prop in entr) {
       if (entr.hasOwnProperty(prop)) {
-        if (prop in this._entries) throw Error(`duplicate property ${prop}`)
-        this._entries[prop] = entr[prop]
+        if (prop in this._entries || prop in this._optional)
+          throw Error(`duplicate property ${prop}`)
+        target[prop] = entr[prop]
       }
     }
     return this
   }
-}
-
-const haveSameKeys = (obj1, obj2) => {
-  if (typeof obj1 !== 'object' || typeof obj2 !== 'object') return false
-  const keys1 = Object.keys(obj1)
-    .slice()
-    .sort()
-  const keys2 = Object.keys(obj2)
-    .slice()
-    .sort()
-  if (keys1.length !== keys2.length) return false
-  for (const i in keys1) if (keys1[i] !== keys2[i]) return false
-  return true
 }
