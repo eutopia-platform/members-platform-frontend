7ae6c819455dbfc4551879accd2e96a62d8f8587
Mathis Bullinger <bullinger.mathis@gmail.com>
2019-06-18 23:40:59 +0200

    [CUBE-46] (feat): schema object add relations

--- src/schema/types/object.js
+++ src/schema/types/object.js
@@ -7,6 +7,7 @@ export default class Object_ extends Type_ {
     this._entries = {}
     this._optional = {}
     this._exclusive = false
+    this._relations = []
   }
 
   check(v) {
@@ -26,6 +27,12 @@ export default class Object_ extends Type_ {
     )
       return false
 
+    const keys = Object.keys(v)
+    for (let e of this._relations) {
+      if (!Object_._rel[e[0]](keys.includes(e[1]), keys.includes(e[2])))
+        return false
+    }
+
     return Object.entries(v).every(e =>
       Object.keys(allChecks).includes(e[0]) ? allChecks[e[0]].check(e[1]) : true
     )
@@ -44,6 +51,14 @@ export default class Object_ extends Type_ {
     return this
   }
 
+  relation(txt) {
+    assert(txt.split(' ').length === 3, 'invalid relation')
+    const [a, rel, b] = txt.split(' ')
+    assert(Object.keys(Object_._rel).includes(rel), `unknown relation "${rel}"`)
+    this._relations.push([rel, a, b])
+    return this
+  }
+
   _addTo(entr, target) {
     assert(typeof entr === 'object')
     assert(
@@ -59,4 +74,10 @@ export default class Object_ extends Type_ {
     }
     return this
   }
+}
+Object_._rel = {
+  OR: (a, b) => a || b,
+  XOR: (a, b) => (a ? !b : b),
+  AND: (a, b) => a && b,
+  NAND: (a, b) => !(a && b),
 }
