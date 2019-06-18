dba76d6e2f4ac4ded74ceb7b311d3d852f0e1cd6
Mathis Bullinger <bullinger.mathis@gmail.com>
2019-06-19 01:03:11 +0200

    [CUBE-46] (feat): schema type convenience methods

--- src/schema/types/index.js
+++ src/schema/types/index.js
@@ -2,6 +2,31 @@ import Type_ from './type'
 import String_ from './string'
 import Number_ from './number'
 import Object_ from './object'
+import Array_ from './array'
 import UUID_ from './uuid'
 
-export { Type_, String_, Number_, Object_, UUID_ }
+const obj = entries =>
+  entries === undefined ? new Object_() : new Object_().entries(entries)
+
+const arr = every =>
+  every === undefined ? new Array_() : new Array_().every(every)
+
+const num = (min = null, max = null) => new Number_().min(min).max(max)
+
+const int = (min = null, max = null) => num(min, max).integer()
+
+const str = () => new String_()
+
+export {
+  Type_,
+  String_,
+  Number_,
+  Object_,
+  Array_,
+  UUID_,
+  obj,
+  arr,
+  num,
+  int,
+  str,
+}
