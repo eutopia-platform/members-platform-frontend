f642c307003e94a44681e6005803bbf089ffa22e
Mathis Bullinger <bullinger.mathis@gmail.com>
2019-06-19 18:03:05 +0200

    [CUBE-46] (feat): add setNested utility function

--- src/scripts/object.js
+++ src/scripts/object.js
@@ -3,3 +3,13 @@ export const getNested = (obj, ...keys) => {
   if (keys.length === 0) return obj
   return getNested(obj[keys.shift()], ...keys)
 }
+
+export const setNested = (obj, value, ...keys) => {
+  if (keys.length === 0) return value
+  return {
+    ...(obj !== undefined && obj),
+    ...{
+      [keys[0]]: setNested(obj[keys[0]] || {}, value, ...keys.slice(1)),
+    },
+  }
+}
