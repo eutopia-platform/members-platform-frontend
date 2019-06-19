e4c5bee78c8e69fa790cb90d2fabea25a00d683b
Mathis Bullinger <bullinger.mathis@gmail.com>
2019-06-19 18:03:26 +0200

    [CUBE-46] (test): add tests for setNested

--- src/scripts/object.test.js
+++ src/scripts/object.test.js
@@ -1,4 +1,4 @@
-import { getNested } from './object'
+import { getNested, setNested } from './object'
 
 describe('getNested', () => {
   const obj = {
@@ -25,5 +25,49 @@ describe('getNested', () => {
   it('returns undefined for paths that are not in the object', () => {
     expect(getNested(obj, 'f', 'o', 'o')).toBe(undefined)
     expect(getNested(obj, 'a', 'b', 'c', 'd')).toBe(undefined)
+  })
+})
+
+describe('setNested', () => {
+  const obj = {
+    a: {
+      b: {
+        c: 'foo',
+      },
+    },
+  }
+  const obj2 = {
+    a: {
+      b: {
+        c: 'foo',
+      },
+    },
+    d: {
+      e: 'bar',
+    },
+  }
+  const obj3 = {
+    a: {
+      b: {
+        c: 'foo',
+      },
+      d: {
+        e: 'bar',
+      },
+    },
+  }
+
+  it('constructs the correct object', () => {
+    expect(setNested({}, 'foo', 'a', 'b', 'c')).toEqual(obj)
+  })
+
+  it("doesn't remove other properties", () => {
+    expect(setNested({ d: { e: 'bar' } }, 'foo', 'a', 'b', 'c')).toEqual(obj2)
+  })
+
+  it("doesn't remove properties that are in path", () => {
+    expect(setNested({ a: { d: { e: 'bar' } } }, 'foo', 'a', 'b', 'c')).toEqual(
+      obj3
+    )
   })
 })
